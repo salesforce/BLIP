a176f1e9cc5a232d2cc6e21b77d2c7e18ceb3c37
root <junnan.li@salesforce.com>
2022-01-29 02:41:17 +0000

    update demo

--- models/blip.py
+++ models/blip.py
@@ -23,7 +23,7 @@ from timm.models.hub import download_cached_file
 class BLIP_Base(nn.Module):
     def __init__(self,                 
                  med_config = 'configs/med_config.json',  
-                 image_size = 384,
+                 image_size = 224,
                  vit = 'base',
                  vit_grad_ckpt = False,
                  vit_ckpt_layer = 0,                 
