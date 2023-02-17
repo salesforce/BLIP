7ab78227d4527736f1a78dd92acff38f1d871e2b
Plachta <liusongting07@gmail.com>
2023-02-17 14:34:54 +0800

    Added model inspect code

---
+++ model_inspect.py
@@ -0,0 +1,47 @@
+'''
+This script is created to be run under DEBUG mode, to inspect the model pipeline & intermediate variables
+Most functions are copied from demo.ipynb
+'''
+
+from PIL import Image
+import requests
+import torch
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# function to load a image as example
+def load_demo_image(image_size, device):
+    img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg'
+    raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+    w, h = raw_image.size
+
+    transform = transforms.Compose([
+        transforms.Resize((image_size, image_size), interpolation=InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    ])
+    image = transform(raw_image).unsqueeze(0).to(device)
+    return image
+
+##% -----------------------------------------------------------------------------
+# Extract image & text features with pretrained encoders
+from models.blip import blip_feature_extractor
+
+image_size = 224
+image = load_demo_image(image_size=image_size, device=device)
+
+model_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base.pth'
+
+model = blip_feature_extractor(pretrained=model_url, image_size=image_size, vit='base')
+model.eval()
+model = model.to(device)
+
+caption = 'a woman sitting on the beach with a dog'
+
+multimodal_feature = model(image, caption, mode='multimodal')[0, 0]
+image_feature = model(image, caption, mode='image')[0, 0]
+text_feature = model(image, caption, mode='text')[0, 0]
+
