364adcfc2592f7613af0d69f8a2fa86f4885f4a6
Plachta <liusongting07@gmail.com>
2023-02-23 13:42:11 +0800

    Added full combiner block, forward & backward passed

--- models/combiner.py
+++ models/combiner.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from timm.models.layers import trunc_normal_, DropPath
 class BiMultiHeadAttention(nn.Module):
     def __init__(self, v_dim, l_dim, embed_dim, num_heads, attn_dropout=0.1, out_dropout=0.1):
         super(BiMultiHeadAttention, self).__init__()
@@ -107,11 +108,150 @@ class BiMultiHeadAttention(nn.Module):
 
             return xv, xl
 
+class Mlp(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=nn.GELU,
+        drop=0.0,
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class CombinerLayer(nn.Module):
+    def __init__(
+            self,
+            v_dim,
+            l_dim,
+            dim,
+            num_heads,
+            mlp_ratio=4.0,
+            drop=0.0,
+            attn_drop=0.0,
+            drop_path=0.0,
+            act_layer=nn.GELU,
+            norm_layer=nn.LayerNorm,
+            layer_scale_init_values=0.1,
+    ):
+        super().__init__()
+        self.norm1_v = norm_layer(v_dim)
+        self.norm1_l = norm_layer(l_dim)
+        self.attn = BiMultiHeadAttention(v_dim=v_dim, l_dim=l_dim, embed_dim=dim, num_heads=num_heads, attn_dropout=attn_drop)
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.norm2_v = norm_layer(v_dim)
+        self.norm2_l = norm_layer(l_dim)
+        mlp_hidden_dim_v = int(v_dim * mlp_ratio)
+        mlp_hidden_dim_l = int(l_dim * mlp_ratio)
+        self.mlp_v = Mlp(
+            in_features=v_dim,
+            hidden_features=mlp_hidden_dim_v,
+            act_layer=act_layer,
+            drop=drop,
+        )
+        self.mlp_l = Mlp(
+            in_features=l_dim,
+            hidden_features=mlp_hidden_dim_l,
+            act_layer=act_layer,
+            drop=drop,
+        )
+
+        self.gamma_1_v = \
+            nn.Parameter(layer_scale_init_values * torch.ones((v_dim)), requires_grad=True) \
+                if layer_scale_init_values is not None else 1.0
+        self.gamma_1_l = \
+            nn.Parameter(layer_scale_init_values * torch.ones((l_dim)), requires_grad=True) \
+                if layer_scale_init_values is not None else 1.0
+        self.gamma_2_v = \
+            nn.Parameter(layer_scale_init_values * torch.ones((v_dim)), requires_grad=True) \
+                if layer_scale_init_values is not None else 1.0
+        self.gamma_2_l = \
+            nn.Parameter(layer_scale_init_values * torch.ones((l_dim)), requires_grad=True) \
+                if layer_scale_init_values is not None else 1.0
+
+    def forward(self, vision_input, language_input, attention_mask_l=None, mode='vl'):
+        assert (
+                mode in ['v', 'l', 'vl']
+        ), f"forward mode must be one of v, l or vl, got {mode} instead."
+        if mode == 'v':
+            vision_input = self.norm1_v(vision_input)
+            vision_output = self.attn(vision_input, None, None, mode=mode)
+            vision_output = self.drop_path(vision_input + self.gamma_1_v * vision_output)
+
+            vision_output = vision_output + self.drop_path(self.gamma_2_v * self.mlp_v(self.norm2_v(vision_output)))
+            return vision_output
+        elif mode == 'l':
+            language_input = self.norm1_l(language_input)
+            language_output = self.attn(None, language_input, attention_mask_l, mode=mode)
+            language_output = self.drop_path(language_input + self.gamma_1_l * language_output)
+
+            language_output = language_output + self.drop_path(self.gamma_2_l * self.mlp_l(self.norm2_l(language_output)))
+            return language_output
+        elif mode == 'vl':
+            vision_input, language_input = self.norm1_v(vision_input), self.norm1_l(language_input)
+            fused_v, fused_l = self.attn(vision_input, language_input, attention_mask_l, mode='vl')
+            fused_v = self.drop_path(vision_input + self.gamma_1_v * fused_v)
+            fused_l = self.drop_path(language_input + self.gamma_1_l * fused_l)
+            return self.norm2_v(fused_v), self.norm2_l(fused_l)
+
+class CombinerBlock(nn.Module):
+    def __init__(
+            self,
+            v_dim,
+            l_dim,
+            dim,
+            num_heads,
+            mlp_ratio=4.0,
+            drop=0.0,
+            attn_drop=0.0,
+            drop_path=0.0,
+            act_layer=nn.GELU,
+            norm_layer=nn.LayerNorm,
+            layer_scale_init_values=0.1,
+    ):
+        super().__init__()
+        self.layer = CombinerLayer(
+            v_dim,
+            l_dim,
+            dim,
+            num_heads,
+            mlp_ratio=4.0,
+            drop=0.0,
+            attn_drop=0.0,
+            drop_path=0.0,
+            act_layer=nn.GELU,
+            norm_layer=nn.LayerNorm,
+            layer_scale_init_values=0.1,
+        )
+    def forward(self, vision_input, language_input, language_mask):
+        # fusion forward
+        v, l = self.layer(vision_input, language_input, language_mask, mode='vl')
+        v = self.layer(v, None, None, mode='v')
+        l = self.layer(None, l, language_mask, mode='l')
+        return v, l
+
+
 
 
 # TODO: for testing purpose only, delete the code below after layers completed
 if __name__ == "__main__":
-    bimhattn = BiMultiHeadAttention(v_dim=1024, l_dim=768, embed_dim=768, num_heads=12)
+    block = CombinerBlock(v_dim=1024, l_dim=768, dim=768, num_heads=12)
     # define input shape
     batchsize = 16
     maximum_text_length = 77
@@ -124,8 +264,15 @@ if __name__ == "__main__":
     language_mask = torch.zeros([maximum_text_length])
     language_mask[actual_text_length:] = -1e10
     # vision self attention test
-    output_v = bimhattn(vision_input, None, mode='v')
-    # language self attention test
-    output_l = bimhattn(None, language_input, language_mask, mode='l')
-    fused_v, fused_l = bimhattn(vision_input, language_input, language_mask, mode='vl')
+    fused_v, fused_l = block(vision_input, language_input, language_mask)
+    v_cls = fused_v[:,-1,:]
+    l_cls = fused_l[:,-1,:]
+    v_proj = nn.Linear(1024, 768)
+    l_proj = nn.Linear(768, 768)
+    v_cls = v_proj(v_cls)
+    l_cls = l_proj(l_cls)
+    dummy = (v_cls + l_cls).mean()
+    dummy.backward()
+    print("test passed")
+
 
